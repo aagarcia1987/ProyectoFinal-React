e5420063c48e9b801f29e5bfc15ebce2d48d880e
aagarcia1987 <adriang1987@gmail.com>
2025-10-30 01:56:30 -0300

    solucion de errores

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,14 @@
 import './NavBar.css'
 import CartWidget from './CartWidget'
 import { Link } from "react-router";
+import logoRiver from "../../assets/logoRiver.png";
 
 export default function NavBar(){
     return(
             <nav className='nav-menu'>
                 <span className='nav-left'>
                     <Link to="/">
-                    <img className="logo" src="./src/assets/logoriver.png" alt="River e-Shop" />
+                    <img className="logo" src={logoRiver} alt="River e-Shop" />
                     </Link>
                 </span>
                 <nav className='nav-center'>
